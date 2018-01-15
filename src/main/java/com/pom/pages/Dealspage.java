7ed9d518f013af9add69c5d8e3430af857aaae3d
pankgupta20 <33956042+pankgupta20@users.noreply.github.com>
2018-01-15 20:42:17 +0000

    new page added

---
+++ src/main/java/com/pom/pages/Dealspage.java
@@ -0,0 +1,11 @@
+package com.pom.pages;
+
+import com.pom.base.DriverScript;
+
+public class Dealspage extends DriverScript {
+
+	public void dealAdd(){
+		System.out.println("Deal Added");
+	}
+	
+}
